5534b93259d727fa1b48bf306537d46e915991e4
mariamlaith <mariamlaith98@gmail.com>
2026-09-23 02:47:59 +0300

    Login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div 
+    :class="[
+      'home-page font-sans min-h-screen transition-colors duration-200',
+      isDarkMode ? 'dark bg-stone-950 text-stone-100' : 'bg-[#faf7f2] text-stone-900'
+    ]"
+    :dir="currentLang === 'ar' ? 'rtl' : 'ltr'"
+  >
+
+      <Login 
+        :is-dark-mode="isDarkMode" 
+        :current-lang="currentLang" 
+      />
+  </div>
+  
+</template>
+
+<script>
+import Login from '@/components/Login.vue'
+
+export default {
+  name: 'LoginView',
+  components: {
+    Login,
+  },
+  props: {
+    isDarkMode: {
+      type: Boolean,
+      default: false
+    },
+    currentLang: {
+      type: String,
+      default: 'ar'
+    }
+  }
+}
+</script>
